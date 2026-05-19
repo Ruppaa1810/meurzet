a95a991bc06f36ffec5d1b7f5d29a72aeb137589
Santiago Rupani <santiagorupani1810@gmail.com>
2026-05-19 12:02:28 -0300

    Feat: Estructura base con Tailwind, Supabase y ambientes configurados

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://yenkuvvumgmuyvjludeg.supabase.co/rest/v1/',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inllbmt1dnZ1bWdtdXl2amx1ZGVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMDc1ODMsImV4cCI6MjA5NDY4MzU4M30.WOnAysNAhia3JubCLut7UkoGhBFWZHjyAS15SHiruT8'
+};
